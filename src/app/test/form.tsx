2197b359bc0940bd2fdc5ee1a6fcb66975db3b51
Oleg <oleg.gordeev@sharp-dev.net>
2024-01-23 17:03:57 +0300

    Add registration form

--- src/app/test/form.tsx
+++ src/app/test/form.tsx
@@ -1,11 +1,21 @@
 'use client';
 
+import { useFormState } from 'react-dom';
 import {Button} from "@nextui-org/button";
+import {Input} from "@nextui-org/react";
 import {register} from '@/app/api/register';
 
 export default function Form() {
+  const [state, formAction] = useFormState<{}, FormData>(async (state, formData: FormData) => {
+    await register();
+    return {};
+  }, {});
 
   return (
-    <Button onClick={() => register()}>Click me</Button>
+    <form className="flex w-full flex-wrap md:flex-nowrap gap-4" action={formAction}>
+      <Input type="email" name="email" isRequired label="Email" placeholder="Enter your email"/>
+      <Input type="password" name="password" isRequired label="Password" placeholder="Enter your password"/>
+      <Button type="submit">Submit</Button>
+    </form>
   )
 }
