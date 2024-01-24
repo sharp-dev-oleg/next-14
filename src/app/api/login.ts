b8630df1ce6edd9081623de712f7ae93fc5acdca
Oleg <oleg.gordeev@sharp-dev.net>
2024-01-24 16:47:01 +0300

    Send login request

--- src/app/api/login.ts
+++ src/app/api/login.ts
@@ -1,1 +1,10 @@
-'use server'
+'use server';
+import {request} from "./request";
+
+export const login = (email: string, password: string) => {
+  const data = {
+    email,
+    password
+  };
+  return request('POST', '/sessions/create', data)
+};
--- src/app/login.tsx
+++ src/app/login.tsx
@@ -2,10 +2,36 @@
 
 import {Button} from "@nextui-org/button";
 import {Input, Chip} from "@nextui-org/react";
+import {login} from "@/app/api/login";
+import {useFormState} from "react-dom";
+
+interface FormState {
+  error: string
+}
 
 export default function Login() {
+  const [state, formAction] = useFormState<FormState, FormData>(async (state, formData: FormData) => {
+    try {
+      const response = await login(
+        formData.get('email') as string,
+        formData.get('password') as string
+      );
+      console.log('response', response);
+      return {
+        error: ''
+      };
+    }
+    catch(e) {
+      return {
+        error: (e as Error)?.message ?? 'Unknown error'
+      }
+    }
+  }, {
+    error: ''
+  });
+
   return (
-    <form className="flex w-full md:w-1/2 flex-wrap gap-4">
+    <form className="flex w-full md:w-1/2 flex-wrap gap-4" action={formAction}>
       <Input type="email" name="email" isRequired label="Email" placeholder="Enter your email"/>
       <Input type="password" name="password" isRequired label="Password" placeholder="Enter your password"/>
       <p className="flex gap-4 items-center">
